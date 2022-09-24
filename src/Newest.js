dbce3f143736c5526a9716aaf92ae7860bec9a1f
Fabian Geiselhart <me@f4814n.de>
2022-09-24 23:56:11 +0200

    feat: Fetch song list from server

--- src/Newest.js
+++ src/Newest.js
@@ -1,65 +1,40 @@
-
-import { Link } from "react-router-dom";
-import { NavbarBottomAdd } from "./NavbarBottomAdd";
-import { Song } from "./Song";
-import React, { useState, useEffect } from 'react';
+import {Link} from "react-router-dom";
+import {NavbarBottomAdd} from "./NavbarBottomAdd";
+import {Song} from "./Song";
+import React, {useState, useEffect} from 'react';
 
 
 export const Newest = () => {
+    const url = `http://49.12.207.165:4000/api/session/1/list`;
 
-
-  const url = `http://49.12.207.165:4000/api/session/1/list`;
-
-  const fetchData = async () => {
-    const res = await fetch(url)
-    const data = await res.json()
-
-    console.log(data)
-
-    return data
-  }
-
-
-
-
-
-  const [songs, setSongs] = useState([])
-
-  useEffect(() => {
-    async function fetchSongs() {
-      const songs = await fetchData()
-      setSongs(songs)
+    const fetchData = async () => {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data)
+        const arrayOfLists = data.map(
+            record => <Song id={record.id} title={record.title} artist={record.artist}/>
+        )
+        return arrayOfLists
     }
 
-    fetchSongs()
-  }, []) ;
 
-  return (
-    <div>
-      <div className="header">
-      
+    const [songs, setSongs] = useState([])
 
+    useEffect(() => {
+        fetchData().then(result => setSongs(result));
+    }, []);
 
-
-
-        <h2 className="queue-headline">Queue: </h2>
-        
-
-
-      </div>
-      <div className="menu">
-
-
-       
-
-        <Song song="Song title" />
+    return (
+        <div>
+            <div className="header">
+                <h2 className="queue-headline">Queue: </h2>
+            </div>
+            <div className="menu">
+                {songs}
+            </div>
+            <NavbarBottomAdd/>
+        </div>
 
 
-
-      </div>
-      <NavbarBottomAdd />
-    </div>
-
-
-  )
+    )
 }
--- src/Song.js
+++ src/Song.js
@@ -1,36 +1,26 @@
 
 import upvoteempty from "./img/arrow.svg";
 import upvotefull from "./img/arrow-fill.svg";
+import {useEffect, useState} from "react";
 
 export const Song = (props) => {
+	const url = `http://49.12.207.165:4000/api/session/1/song/{props.id}`;
+
+	const voteHandler = () => {
+		console.log("Upvote ", props.id)
+	}
 
 	return (
-
-
-
-
 		<div className="song">
-
-
-			<p className="song-name"><strong>{props.song}</strong>  </p>
-			<div className="right">
-
-
+			<div className="song-info">
+				<p className="song-name"><strong>{props.title}</strong>  </p>
+				<p className="song-artist">{props.artist}</p>
+			</div>
+			<div className="right" onClick={voteHandler}>
 				<img alt="vote"  className="upvote-button" src={upvoteempty} />
-
-
-				
-					<h1 className="voting-counter">0</h1>
-				
-
+				<h1 className="voting-counter">0</h1>
 			</div>
 		</div>
-
-
-
 	)
-
-
-
 }
 
